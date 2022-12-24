f0a8d44672f074fe42f0b72cf5b74bc2beb4dd33
Danny Rangel <danielrangel.tech@gmail.com>
2022-12-23 21:31:58 -0800

    fix responsive appbar layout

--- components/AppBar.tsx
+++ components/AppBar.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function AppBar() {
   return (
-    <nav className="bg-transparent flex h-12 px-2 container items-center text-md m-auto">
+    <nav className="bg-transparent flex h-10 px-2 pt-6 pb-4 sm:py-8 md:py-10 container items-center text-md m-auto">
       <Image src="/logo.png" alt="favz" width={24} height={24}/>
       <div className="flex items-center text-white text-2xl font-sans">
         favz
